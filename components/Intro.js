871d175af98f79b3636dd4839752e19fbc38725e
Ferran Basora <fcsonline@gmail.com>
2020-09-11 09:47:04 +0000

    Fix typo

--- components/Intro.js
+++ components/Intro.js
@@ -13,7 +13,7 @@ export default function Intro() {
       <p className="text-base leading-7 mb-12">
         On this site I'm going to post technical articles related
         to software engineering and other interesting stuff.
-        An attempt to present ans express my thoughs about any kind of topic
+        An attempt to present and express my thoughs about any kind of topic
         about the world arround us.
       </p>
     </div>
