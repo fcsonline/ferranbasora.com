7201bd8694df5911d9f9d069078237cb59f551c1
Ferran Basora <fcsonline@gmail.com>
2020-09-11 09:55:24 +0000

    Small tweaks

--- site.config.js
+++ site.config.js
@@ -5,7 +5,7 @@ export default {
       name: "Ferran Basora",
       summary: "We need a tool™ #rust, #opensource, #security, #webperf",
     },
-    description: "A blog created with Next.js and Tailwind.css",
+    description: "The blog, projects, and other stuff of Ferran Basora",
     siteUrl: "https://www.ferranbasora.com/",
     language: "en-US",
     social: {
