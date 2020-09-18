56471a3d7eb5f9cad34c58c306c4e0a8bbe84937
Ferran Basora <fcsonline@gmail.com>
2020-09-18 23:20:26 +0000

    Change react syntax hightlighter

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,5 +1,6 @@
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export default function CodeBlock ({ language, value }) {
-  return <SyntaxHighlighter language={language}>{value}</SyntaxHighlighter>;
+  return <SyntaxHighlighter style={materialDark} language={language}>{value}</SyntaxHighlighter>;
 };
