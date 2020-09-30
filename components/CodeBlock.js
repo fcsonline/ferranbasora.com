9910c288ba01e671dda8054d86c5b15c1c3d9c6b
Ferran Basora <fcsonline@gmail.com>
2020-09-30 16:16:27 +0000

    Code intersector

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,6 +1,13 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { useInView } from 'react-intersection-observer';
 
 export default function CodeBlock ({ language, value }) {
-  return <SyntaxHighlighter style={materialDark} language={language}>{value}</SyntaxHighlighter>;
+  const { ref, inView, entry } = useInView({});
+
+  return (
+    <div ref={ref}>
+      <SyntaxHighlighter style={materialDark} language={language}>{value}</SyntaxHighlighter>
+    </div>
+  );
 };
