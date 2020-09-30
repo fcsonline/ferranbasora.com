f3ddd0f8ac88cac373f11fdf21ae3533feb24163
Ferran Basora <fcsonline@gmail.com>
2020-09-30 16:11:23 +0000

    Offline PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withPlugins = require("next-compose-plugins");
+const withOffline = require('next-offline')
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([optimizedImages]);
+module.exports = withPlugins([withOffline, optimizedImages]);
