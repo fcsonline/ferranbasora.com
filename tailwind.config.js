36db13bf93e1783039cd4dc27ce3bd39b8c7e72a
Ferran Basora <fcsonline@gmail.com>
2020-11-23 18:18:36 +0000

    Remove backticks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,59 +1,65 @@
 const defaultSans = [
-  "system-ui",
-  "-apple-system",
-  "BlinkMacSystemFont",
+  'system-ui',
+  '-apple-system',
+  'BlinkMacSystemFont',
   '"Segoe UI"',
-  "Roboto",
+  'Roboto',
   '"Helvetica Neue"',
-  "Arial",
+  'Arial',
   '"Noto Sans"',
-  "sans-serif",
+  'sans-serif',
   '"Apple Color Emoji"',
   '"Segoe UI Emoji"',
   '"Segoe UI Symbol"',
-  '"Noto Color Emoji"',
-];
+  '"Noto Color Emoji"'
+]
 
 const defaultSerif = [
-  "Georgia",
-  "Cambria",
+  'Georgia',
+  'Cambria',
   '"Times New Roman"',
-  "Times",
-  "serif",
-];
+  'Times',
+  'serif'
+]
 
 module.exports = {
-  purge: ["./**/{pages,components,tailwind}/**/*.{js,jsx,ts,tsx,css}"],
+  purge: ['./**/{pages,components,tailwind}/**/*.{js,jsx,ts,tsx,css}'],
   theme: {
     extend: {
       fontSize: {
-        "7xl": "4.5rem",
+        '7xl': '4.5rem'
       },
       spacing: {
-        "14": "3.375rem",
-      },
+        14: '3.375rem'
+      }
     },
     fontFamily: {
-      display: ["Inter", ...defaultSans],
-      body: ["Inter", ...defaultSerif],
+      display: ['Inter', ...defaultSans],
+      body: ['Inter', ...defaultSerif]
     },
     typography: (theme) => ({
       default: {
         css: {
-          color: theme("colors.gray.900"),
+          color: theme('colors.gray.900'),
           blockquote: {
-            borderLeftColor: theme("colors.gray.700"),
+            borderLeftColor: theme('colors.gray.700')
           },
-          "ol > li::before": {
-            color: theme("colors.gray.700"),
+          'code::before': {
+            content: ''
           },
-          "ul > li::before": {
-            backgroundColor: theme("colors.gray.700"),
+          'code::after': {
+            content: ''
           },
-        },
-      },
-    }),
+          'ol > li::before': {
+            color: theme('colors.gray.700')
+          },
+          'ul > li::before': {
+            backgroundColor: theme('colors.gray.700')
+          }
+        }
+      }
+    })
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
-};
+  plugins: [require('@tailwindcss/typography')]
+}
