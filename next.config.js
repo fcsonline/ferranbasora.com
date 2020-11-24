d117822765b0fc9793fdd9721a0ddf3559fd94f8
Ferran Basora <fcsonline@gmail.com>
2020-11-24 17:49:49 +0000

    Remove old image optimization

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins')
 const withOffline = require('next-offline')
-const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([withOffline, optimizedImages]);
+module.exports = withPlugins([withOffline])
