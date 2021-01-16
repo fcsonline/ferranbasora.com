6c041e2a8d58887e41f078b5416e4e032ea2defc
Ferran Basora <fcsonline@gmail.com>
2021-01-16 16:42:13 +0000

    Fix glitch with intersection-observer

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { PropTypes } from 'prop-types'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { materialDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
@@ -5,10 +6,15 @@ import { useInView } from 'react-intersection-observer'
 
 const CodeBlock = ({ language, value }) => {
   const { ref, inView } = useInView({})
+  const [hasBeenInView, setHasBeenInView] = React.useState(false)
+
+  React.useEffect(() => {
+    setHasBeenInView(hasBeenInView || inView)
+  }, [inView])
 
   return (
     <div ref={ref}>
-      { inView && (<SyntaxHighlighter style={materialDark} language={language}>{value}</SyntaxHighlighter>) }
+      { hasBeenInView && (<SyntaxHighlighter style={materialDark} language={language}>{value}</SyntaxHighlighter>) }
     </div>
   )
 }
