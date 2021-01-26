4785e58cda2b992b02337ad97c8d874c452ac511
Ferran Basora <fcsonline@gmail.com>
2021-01-26 22:46:32 +0000

    Fix nextjs config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@ const withPlugins = require('next-compose-plugins')
 const withOffline = require('next-offline')
 const withMDX = require('@next/mdx')
 
-
-module.exports = withPlugins([withOffline, withMDX, {
+module.exports = withPlugins([withOffline, withMDX], {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     // without this we get
@@ -18,4 +17,4 @@ module.exports = withPlugins([withOffline, withMDX, {
     }
     return config
   }
-}])
+})
